557531c642a40cc947c73d22359f8847881f1574
Carlos Vanzetto <vanzettoc@gmail.com>
2022-03-15 10:05:06 -0300

    clase 03 terminada

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const btn = document.querySelector("[data-form-btn]");
-const createTask = e=>{
+const createTask = e => {
   e.preventDefault();
+  const lista = document.querySelector("[data-list]");
   const input = document.querySelector("[data-form-input]");
-  let nuevaTarea = input.value;
-  console.log(nuevaTarea);
-  }
+  const value = input.value;
+  const task = document.createElement("li");
+  task.classList.add("card");
+  const content = `<div>
+  <i class="far fa-check-square icon"></i>
+  <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML = content;
+  lista.appendChild(task);
+  input.value = "";
   
+
+}
+
 btn.addEventListener("click", createTask);
